2a36740ca6ebefb9e0801490d319b466689fca5b
Sergey Shcheglov <97shcheglov@gmail.com>
2022-01-27 17:14:53 +0600

    Add RectangleView, ArrowView

---
+++ Drawing/ArrowView.swift
@@ -0,0 +1,20 @@
+//
+//  ArrowView.swift
+//  Drawing
+//
+//  Created by Sergey Shcheglov on 27.01.2022.
+//
+
+import SwiftUI
+
+struct ArrowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ArrowView_Previews: PreviewProvider {
+    static var previews: some View {
+        ArrowView()
+    }
+}
